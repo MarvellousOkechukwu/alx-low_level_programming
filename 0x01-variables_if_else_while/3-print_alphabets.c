b00f6f9d1768aa015dcba7f445a849e1a1e984bc
MarvellousOkechukwu <marvellous.okechukwu@gmail.com>
2022-09-11 18:02:00 -0700

    alphaBET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,12 +11,12 @@ int main(void)
 char ch;
 for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-	putchar (ch) ;
+	putchar (ch);
 }
 for (ch = 'A' ; ch <= 'z' ; ch++)
 {
-        putchar (ch) ;
+        putchar (ch);
 }
-putchar('\n') ;
-return (0) ;
+putchar('\n');
+return (0);
 }
