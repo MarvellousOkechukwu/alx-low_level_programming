6927533ace099b72c75d7eca90ca8976894d2f01
MarvellousOkechukwu <marvellous.okechukwu@gmail.com>
2022-09-15 08:00:53 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,11 +5,11 @@
  * Return: 0
  */
 
-int main(void)
-{
-int i;
+ int main(void)
+ {
+ int i;
 
-printf("infinite loop incoming :(\n");
+ printf("infinite loop incoming :(\n");
 
 i = 0;
 /*
@@ -18,7 +18,7 @@ i = 0;
  * putchar(i);
  * }
  */
-printf("infinte loop avoided! \\o/\n");
+ printf("infinte loop avoided! \\o/\n");
 
-return (0);
-}
+ return (0);
+ }
