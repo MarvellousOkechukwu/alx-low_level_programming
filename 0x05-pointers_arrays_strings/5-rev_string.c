fa24c5a2e7131240783ccee852ecd17113522121
MarvellousOkechukwu <marvellous.okechukwu@gmail.com>
2022-09-19 07:59:49 -0700

    hot fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,7 +9,7 @@
 
 void rev_string(char *s)
 {
-	"sam"->"mas"
+	"sam" -> i"mas"
 	char tmp;
 	int i, length1, length2;
 
@@ -22,7 +22,6 @@ void rev_string(char *s)
 	length2 = length1 - 1;
 	for (i = 0; i < length1 / 2; i++)
 	{
-		"samalx" "xlamas"
 		tmp = s[i];
 		s[i] = s[length2];
 		s[length2] = tmp;
