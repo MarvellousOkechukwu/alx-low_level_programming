8211d7aacb43defa07d010499acfc743e5da597e
MarvellousOkechukwu <marvellous.okechukwu@gmail.com>
2022-09-22 07:23:22 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,25 +1,24 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdio.h>
 /**
- * File: main.h
- * Auth: Marvellous
- * Desc: Header file containing prototypes of all functions
- * 	used in the 0x06. C - More pointers, arrays and strings directory.
+ * void prototype ()
+ * int prototype ()
+ * char prototype ()
  */
-
-int _Putchar(char c);
+int _putchar(char c);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+void print_number(int n);
+void print_buffer(char *b, int size);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
-void print_number(int n)
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
+char *add_string(char *n1, char *n2, char *r, int r_index);
 
-#endif
+#endif /*MAIN_H*/
