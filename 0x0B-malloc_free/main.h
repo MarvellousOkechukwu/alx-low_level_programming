99f810af0b4dc1cda267c49b0bbe97b59f2e91bf
MarvellousOkechukwu <marvellous.okechukwu@gmail.com>
2022-10-05 10:13:41 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,7 +7,7 @@ int _putchar(char);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
-int **alloc_grid(int ac, char **av);
+char *argstostr(int ac, char **av);
 int **alloc_grid(int width, int height);
 char **strtow(char *str);
 
