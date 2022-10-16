d327376bc5122894254b1acd73374caf660716f1
MarvellousOkechukwu <marvellous.okechukwu@gmail.com>
2022-10-15 17:57:34 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -16,9 +16,16 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		printf("Error\n");
-		exit(2);
+		exit(1);
 	}
 
+	nbytes = atoi(argv[1]);
+
+	if (nbytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 	for (i = 0; i < nbytes; i++)
 	{
 		printf("%02x", opc[i] & 0xFF);
