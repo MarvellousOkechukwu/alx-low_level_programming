4b701ddc9d0dd1e5535f8a13d8570955e5a4aa73
MarvellousOkechukwu <marvellous.okechukwu@gmail.com>
2022-10-15 18:30:31 -0700

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -15,10 +15,35 @@ int op_add(int a, int b)
 /**
  * op_sub - subtracts two numbers.
  * @a: first number.
- *  @b: second number.
- *  Return: difference.
+ * @b: second number.
+ *
+ * Return: difference.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: multiplication.
  */
 int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: division.
+ */
+int op_div(int a, int b)
 {
 	if (b == 0)
 	{
@@ -30,7 +55,7 @@ int op_mul(int a, int b)
 
 /**
  * op_mod - calculates the module of two numbers.
- * @a: first number.
+ * a: first number.
  * @b: second number.
  *
  * Return: remainder of the division.
