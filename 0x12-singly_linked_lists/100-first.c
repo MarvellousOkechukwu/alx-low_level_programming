e43c6b15850947dc0afc6336de0a6a973018ad0f
MarvellousOkechukwu <marvellous.okechukwu@gmail.com>
2022-10-20 09:05:56 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,6 @@ void __attribute__((constructor)) hare(void);
  */
 void hare(void)
 {
-	printf("You 're beat and yet, you must allow,"
-			"\n1 bore my house upon my back!\n");
+	printf("You 're beat and yet, you must allow,");
+	Printf("\n1 bore my house upon my back!\n");
 }
